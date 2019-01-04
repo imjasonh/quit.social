13cbd72c68a8009a2c6d04f20af625b614550f60
Jason Hall <jasonhall@google.com>
2019-01-04 09:39:14 -0500

    Rephrase, fix upload.sh, always secure

--- upload.sh
+++ upload.sh
@@ -1,14 +1,3 @@
 #!/bin/bash
 
-BUCKET=www.quit.social
-
-gsutil defacl set public-read gs://$BUCKET/
-gsutil -m acl set public-read gs://$BUCKET/*
-
-for i in twitter facebook instagram; do
-  gsutil -h "Content-Type: text/html" cp $i gs://$BUCKET/
-done
-
-for type in css html js json png; do
-  gsutil cp *.$type gs://$BUCKET/
-done
+gcloud --project=quitsocial app deploy -q
