775fea90a6c52c565b7b59fa500b69b0212c8e49
Jason Hall <jason@chainguard.dev>
2024-04-15 09:46:34 -0400

    add slack
    Signed-off-by: Jason Hall <jason@chainguard.dev>

--- antisocial.js
+++ antisocial.js
@@ -1,24 +1,24 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('service-worker.js')
-      .then(function(reg) { console.log('Registered Service Worker'); })
-      .catch(function(err) { console.error('Error registering Service Worker:', err); });
+    .then(function (reg) { console.log('Registered Service Worker'); })
+    .catch(function (err) { console.error('Error registering Service Worker:', err); });
 }
 
 var visits = window.localStorage.getItem(SITE);
 if (visits == null || visits == '') { visits = '[]'; }
 visits = JSON.parse(visits);
 var now = new Date().getTime();
-var cutoff = now - 7*24*60*60*1000; // One week ago.
-visits = visits.filter(function(v) { return v > cutoff; });
+var cutoff = now - 7 * 24 * 60 * 60 * 1000; // One week ago.
+visits = visits.filter(function (v) { return v > cutoff; });
 visits.push(now);
 window.localStorage.setItem(SITE, JSON.stringify(visits));
 
 var count = visits.length;
 var times = '' + count + ' times';
 if (count == 1) { times = ' one time'; }
-document.getElementById('text').innerText = 'You have tried to visit ' + SITE + ' ' +times+' in the last week.';
+document.getElementById('text').innerText = 'You have tried to visit ' + SITE + ' ' + times + ' in the last week.';
 
-document.getElementById('reset').onclick = function() {
+document.getElementById('reset').onclick = function () {
   window.localStorage.clear(SITE);
   document.getElementById('text').innerText = 'You have not tried to visit ' + SITE + ' in the last week.\nKeep it up!';
   document.getElementsByTagName('body')[0].classList.add('okay');
--- service-worker.js
+++ service-worker.js
@@ -1,34 +1,38 @@
-self.addEventListener('install', function(e) {
-  e.waitUntil(caches.open('quit.social').then(function(cache) {
+self.addEventListener('install', function (e) {
+  e.waitUntil(caches.open('quit.social').then(function (cache) {
     return cache.addAll([
-        '/',
-        '/style.css',
-        '/antisocial.js',
+      '/',
+      '/style.css',
+      '/antisocial.js',
 
-        '/facebook',
-        '/facebook.png',
-        '/facebook_manifest.json',
+      '/facebook',
+      '/facebook.png',
+      '/facebook_manifest.json',
 
-        '/twitter',
-        '/twitter.png',
-        '/twitter_manifest.json',
+      '/twitter',
+      '/twitter.png',
+      '/twitter_manifest.json',
 
-        '/instagram',
-        '/instagram.png',
-        '/instagram_manifest.json',
+      '/instagram',
+      '/instagram.png',
+      '/instagram_manifest.json',
 
-        '/snapchat',
-        '/snapchat.png',
-        '/snapchat_manifest.json',
+      '/slack',
+      '/slack.png',
+      '/snapchat_manifest.json',
+
+      '/snapchat',
+      '/snapchat.png',
+      '/snapchat_manifest.json',
     ]);
   }));
 });
 
 
-self.addEventListener('fetch', function(event) {
+self.addEventListener('fetch', function (event) {
   console.log(event.request.url);
   event.respondWith(
-      caches.match(event.request).then(function(response) {
-        return response || fetch(event.request);
-      }));
+    caches.match(event.request).then(function (response) {
+      return response || fetch(event.request);
+    }));
 });
